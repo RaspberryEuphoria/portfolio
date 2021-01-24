9016972480028ee59eaebc9b0b637efba58e5112
RaspberryEuphoria <manon.dev.mion@gmail.com>
2021-01-24 18:19:12 +0100

    feat: add content to the Experience page

--- src/containers/ExperienceContainer/ExperienceContainer.js
+++ src/containers/ExperienceContainer/ExperienceContainer.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import sections from './experiences.json';
+
 
 const ExperienceContainer = () => {
-  return <div></div>;
+  return <div>
+      {sections.map(({ title, stack, duration, description, tasks }) => (
+        <React.Fragment key={title}>
+          <h2>{title}<span>{duration}</span></h2>
+          <p dangerouslySetInnerHTML={{__html: description}}></p>
+
+          <ul className="inline-list">
+            <li className="list-title"><strong>Technology Stack</strong>:</li>
+            {stack.map(({ id, title }) => (
+              <li key={id}>{title}</li>
+            ))}
+          </ul>
+
+          <ul className="regular-list">
+            {tasks.map(({ id, description }) => (
+              <li key={id} dangerouslySetInnerHTML={{__html: description}}></li>
+            ))}
+          </ul>
+        </React.Fragment>
+      ))}
+  </div>;
 };
 
 export default ExperienceContainer;
