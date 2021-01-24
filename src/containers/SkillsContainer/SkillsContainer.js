a2836db1d4844bd3020a2d44668b27b6e3dd2d16
RaspberryEuphoria <manon.dev.mion@gmail.com>
2021-01-24 17:53:13 +0100

    style: use specific className for regular lists

--- src/containers/SkillsContainer/SkillsContainer.js
+++ src/containers/SkillsContainer/SkillsContainer.js
@@ -4,22 +4,25 @@ import sections from './skills.json';
 const SkillsContainer = () => {
   return (
     <div>
-      {sections.map(({ title, skills }) => (
+      {sections.map(({ title, skills }, i) => (
         <React.Fragment key={title}>
           <h2>{title}</h2>
-          <ul className="inline-list">
+          <ul className={i === sections.length - 1 ? 'inline-list' : 'regular-list'}>
             {skills.map(({ id, title }) => (
               <li key={id}>{title}</li>
             ))}
           </ul>
         </React.Fragment>
       ))}
+
       <hr />
+
       <p>
         Beside these skills, I'm particulary fond of functionnal programming, writing clean code,
         refactoring, QA, and SCRUM methodology. I'm always eager to learn new aspects of
         programming!
       </p>
+
       <p>
         While coding is my passion, I think being a good developer is more than just being a "
         <em>code monkey</em>". This is why being involved in every aspects of the conception and
